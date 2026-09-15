589e9d7a28f6b505530ddf02d5a403f22b29a3f7
Sarthak Pani <realsarthutrap1@users.noreply.github.com>
2026-09-15 22:57:00 +0000

    lab9: read aqi file

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 // COMSC-210 | Lab 9 | Sarthak Pani
 #include <iostream>
+#include <fstream>
 #include <array>
 using namespace std;
 
@@ -8,6 +9,11 @@ const int DAYS = 31;
 int main() {
     // San Ramon CA daily AQI, 31 days
     array<int, DAYS> aqi{};
+
+    ifstream in("aqi.txt");
+    for (int i = 0; i < DAYS; i++) {
+        in >> aqi[i];
+    }
 
     return 0;
 }
